c667c96d08178f5994301fc66ca7b0417ac6baef
Gino Ureta <ing.gino@gmail.com>
2022-03-28 06:53:27 -0400

    initial configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'a2-03-project1';
-}
+export class AppComponent {}
