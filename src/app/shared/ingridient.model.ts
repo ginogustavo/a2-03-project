7222092bdd3585f317ff0ca4fe4c2fe544bbff1f
Gino Ureta <ing.gino@gmail.com>
2022-03-29 07:19:26 -0400

    adding models

---
+++ src/app/shared/ingridient.model.ts
@@ -0,0 +1,10 @@
+export class Ingridient {
+  //   public name: string;
+  //   public amount: number;
+
+  // Adding an accessor in front of the variable -> creates the variable
+  constructor(public name: string, public amount: number) {
+    // this.name = name;
+    // this.amount = amount;
+  }
+}
